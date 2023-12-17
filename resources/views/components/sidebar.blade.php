35310920e3d8d85b0c8bd4a0f120d74274e5836b
Adi Prasetia <adiprasetia1@gmail.com>
2023-12-17 21:13:46 +0700

    tes1

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">POS ADI PRASETIA</a>
+            <a href="index.html">POS ADI </a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
